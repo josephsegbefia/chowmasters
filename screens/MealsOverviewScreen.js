aa8f31d0b27bff9ca8c2aa2df22c09b606a64660
Joseph Segbefia <elorma.segbefia@gmail.com>
2024-05-20 13:44:10 +0200

    Add navigation- stack

---
+++ screens/MealsOverviewScreen.js
@@ -0,0 +1,34 @@
+import { View, FlatList, StyleSheet } from "react-native";
+import { MEALS } from "../data/dummy-data";
+import MealItem from "../components/MealItem";
+
+function MealsOverViewScreen({ route }) {
+  const catId = route.params.categoryId;
+
+  const displayedMeals = MEALS.filter((mealItem) => {
+    return mealItem.categoryIds.indexOf(catId) >= 0;
+  });
+
+  function renderMealItem(itemData) {
+    return <MealItem title={itemData.item.title} />;
+  }
+
+  return (
+    <View>
+      <FlatList
+        data={displayedMeals}
+        keyExtractor={(item) => item.id}
+        renderItem={renderMealItem}
+      />
+    </View>
+  );
+}
+
+export default MealsOverViewScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 16,
+  },
+});
